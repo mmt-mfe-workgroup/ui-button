340721921812c603f29baccfdacee0a464740ee0
JC <jeanchristophe.nicolas@vodafone.com>
2024-01-23 16:29:27 +0000

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
+import federation from "@originjs/vite-plugin-federation";
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const mfe = () => federation({
+    name: 'remote-app',
+    filename: 'remoteEntry.js',
+    // Modules to expose
+    exposes: {
+        './App': './src/RemoteApp',
+    },
+    shared: ['react', 'react-dom']
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), mfe()],
   css: {
     postcss: './postcss.config.js',
   },
+  build: {
+    target: 'esnext'
+  }
 })
