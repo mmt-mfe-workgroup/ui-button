df3cdcd2f95a14180ec981ab81946d37b695dc60
JC <jeanchristophe.nicolas@vodafone.com>
2024-01-29 14:29:06 +0000

    update button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,17 @@
 import { FC, MouseEvent } from 'react'
 
 interface ButtonProps {
-  onClick: (event: MouseEvent<HTMLButtonElement>) => void
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void
   label: string
+  type?: 'button' | 'submit'
   disabled?: boolean
 }
 
-const Button: FC<ButtonProps> = ({ onClick, label, disabled }) => {
+const Button: FC<ButtonProps> = ({ onClick, label, disabled, type = 'button' }) => {
   return (
     <button
       onClick={onClick}
-      type="button"
+      type={type}
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       disabled={disabled}
     >
